195170172abbc6163357fb7557179ac38022ce4b
tam nguyen <ndtam.273@gmail.com>
2020-04-08 22:49:38 +0700

    yargs command

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+// customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title'
+        }
+    },
+    handler: function (argv) {
+        console.log('Adding a new note', argv)
+    }
+})
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note')
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+console.log(yargs.argv)
