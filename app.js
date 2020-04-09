9c7e60526951d7c517ad27d1531f650bc0a03856
tam nguyen <ndtam.273@gmail.com>
2020-04-09 11:56:48 +0700

    aygrs

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ yargs.command({
     describe: 'Add a new note',
     builder: {
         title: {
-            describe: 'Note title'
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
         }
     },
     handler: function (argv) {
-        console.log('Adding a new note', argv)
+        console.log('Title:' + argv.title)
+        console.log('Body:' + argv.body)
     }
 })
 
@@ -46,4 +54,5 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+// console.log(yargs.argv)
+yargs.parse()
