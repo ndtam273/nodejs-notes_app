b6e32ce367b572242cfc2597ecb8099f33950576
tam nguyen <ndtam.273@gmail.com>
2020-04-10 11:59:38 +0700

    remove note

--- notes.js
+++ notes.js
@@ -1,4 +1,6 @@
+const chalk = require('chalk')
 const fs = require("fs");
+
 const getNotes = function () {
   return "Your notes...";
 };
@@ -14,16 +16,16 @@ const addNote = function (title, body) {
       body: body,
     });
     saveNotes(notes);
-    console.log("New note added!");
+    console.log(chalk.green.inverse('New note added!'));
   } else {
-    console.log("Note title taken!");
+    console.log(chalk.red.inverse('Note title taken!'));
   }
 }
 
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
-};
+}
 
 const loadNotes = function () {
   try {
@@ -34,7 +36,22 @@ const loadNotes = function () {
     return [];
   }
 };
+
+const removeNote = function (title) {
+  const notes = loadNotes()
+  const afterRemoveNotes = notes.filter(function (note) {
+    return note.title !== title;
+  });
+  
+  if (afterRemoveNotes.length === notes.length) {
+    console.log(chalk.red.inverse('title not exists'))
+  } else {
+    saveNotes(afterRemoveNotes)
+    console.log(chalk.green.inverse('title removed!'))
+  }
+}
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
+  removeNote: removeNote
 };
