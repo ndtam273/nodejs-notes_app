8da00f6d52fff67341169c24b0b6b2739755d72d
tam nguyen <ndtam.273@gmail.com>
2020-04-21 18:00:27 +0700

    destructuring tutorial

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -10,3 +10,27 @@ const user = {
 }
 
 console.log(user)
+
+// Object destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// console.log(product.label)
+// console.log(product.stock)
+
+// const {label: productLabel, stock, rating = 5  } = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
